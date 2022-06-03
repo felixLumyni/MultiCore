3066b0295565b89cf8f214a2b7acb2902cd8e8a8
Lumyni <24883013+felixLumyni@users.noreply.github.com>
2022-06-03 18:28:33 -0400

    placeholder item thing

--- src/main/java/net/fabricmc/multicore/multicore.java
+++ src/main/java/net/fabricmc/multicore/multicore.java
@@ -1,6 +1,11 @@
 package net.fabricmc.multicore;
 
 import net.fabricmc.api.ModInitializer;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.registry.Registry;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -10,6 +15,9 @@ public class multicore implements ModInitializer {
 	// That way, it's clear which mod wrote info, warnings, and errors.
 	public static final Logger LOGGER = LoggerFactory.getLogger("modid");
 
+	//extra item stuff
+	public static final Item MUD_BALL = new Item(new Item.Settings().group(ItemGroup.MISC));
+
 	@Override
 	public void onInitialize() {
 		// This code runs as soon as Minecraft is in a mod-load-ready state.
@@ -17,5 +25,10 @@ public class multicore implements ModInitializer {
 		// Proceed with mild caution.
 
 		LOGGER.info("Hello Fabric world!");
+		multicoreInit();
+	}
+	public void multicoreInit() {
+		Registry.register(Registry.ITEM, new Identifier("multicore","mud_ball"), MUD_BALL);
+		LOGGER.info("Multicore mod initialized!");
 	}
 }
